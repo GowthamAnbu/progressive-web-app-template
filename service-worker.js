fe0cdffe2af0f9ad4e22a08bac7f2f9b236e4280
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2018-01-17 14:23:50 +0530

    updated service worker with activated

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
 var cacheName = 'weatherPWA-step-6-1';
 var filesToCache = [];
 
+/**
+ * all the storage related things go here 
+ * things during the installation of the service worker
+*/
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
@@ -9,4 +13,12 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
+});
+
+/**
+ * all the things like fetching data from cache or network,
+ * deleting the cache data goes here
+*/
+self.addEventListener('activate', function(e) {
+	console.log('[ServiceWorker] Activate');
 });
