cc76696e1a33f3b339cc860b4b0300f43d38ee48
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2018-01-22 14:28:04 +0530

    updated the simple button and configuration with clear proper code  CLEARED THE PREVIOUS FAULT in commit (UNKNOWN CODE IN previous commit)

--- scripts/push-notification.js
+++ scripts/push-notification.js
@@ -1,8 +1,8 @@
 var pubKey = 'BCbDQlXsCxH5cauMXIYCLHlWc0-jgc35BU0p_sVw9bTmkuRurwuDbuVzBM_5xqkL3ku8rmh2uOPKOREvgLoEkGI';
 // yCO2UzZrSHNZJ4Khb50AlzsjZ6hUpVYjFQ34mrpJIss
 
-var notifyBtn = document.getElementById('notify-btn')//,
-    /* btnIcon = notifyBtn.children[0] */; // Original code
+var notifyBtn = document.getElementById('notify-btn'),
+    btnIcon = notifyBtn.children[0];
 
 if ( 'serviceWorker' in navigator && 'PushManager' in window ) {
     notifyBtn.classList.remove('hidden');
@@ -11,35 +11,20 @@ if ( 'serviceWorker' in navigator && 'PushManager' in window ) {
        instead they have requestPermission tbh I don't event know why this works :(
        and even the requestPermission is not working
     */
+    // conditional statment is to check the permission
     if (Notification.permission !== 'denied') {
-        console.log('if');
-        notifyBtn.removeAttribute('disabled');
-    }/* else {
-        notifyBtn.setAttribute('disabled', true);
-        console.log('else');
-    } */ // this code is not needed since I have added the disabled attribute directly to the html
+        notifyBtn.disabled = false;
+    }
 
     navigator.serviceWorker.ready
     .then( sw => {
         // sp.sw = sw; // saving globally for other purposes /* error since no sp is found */
 
-        /* have to check the conditional statement
-           for isSubscribed mainly the setAttribute and the removeAttribute
-        */
-        /* UNCOMMENT the else part of isSubscribed for real code;
-           COMMENTED it testing purposes actually the real code
-           does something else related to unicode YUCK
-        */
         sw.pushManager.getSubscription()
         .then( s => {
             var isSubscribed = s !== null;
             console.log('value is: ', isSubscribed);
-            if (isSubscribed) {
-                notifyBtn.setAttribute('disabled', true);
-            }/* else {
-                notifyBtn.removeAttribute('disabled');
-            } */
-            // btnIcon.innerHtml = isSubscribed ? '&#xf1f6' : '&#xf0f3'; // Original code
+            btnIcon.innerHtml = isSubscribed ? '&#x2606' : '&#x2605'; // this is to add the icon change it has nothing to do with permission
         })
     })
 }
